3865336ede241fe4a455fbfc6fb854ec53844a9c
VasiliyLvov <vasiliy.a.lvov@gmail.com>
2024-05-19 12:28:40 +0500

    20240519_001

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import asyncio
 import config
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
 import logging
 import random
+from keyboards import keyboard_001, keyboard_empty
+import subrcs
 
 #Логирование
 logging.basicConfig(level=logging.INFO)
@@ -13,31 +15,41 @@ bot = Bot(token=config.token)
 dp = Dispatcher()
 
 @dp.message(Command(commands=['start']))
-@dp.message(Command(commands=['старт']))
+@dp.message(F.text.lower() == 'старт')
+@dp.message(F.text.lower() == 'начать')
 async def start(message: types.Message):
     await message.answer(f'Привет, друг {message.from_user.full_name}!\n'
-                         f'Примените команду /info для получения более подробной информации')
+                         f'Примени команду /info для получения более подробной информации', reply_markup=keyboard_001)
 
 @dp.message(Command(commands=['stop']))
-@dp.message(Command(commands=['стоп']))
+@dp.message(F.text.lower() == 'стоп')
+@dp.message(F.text.lower() == 'закончить')
 async def start(message: types.Message):
-    await message.answer(f'Пока, {message.from_user.full_name}!')
+    await message.answer(f'Пока, {message.from_user.full_name}!', reply_markup=keyboard_empty)
 
 @dp.message(Command(commands=['info']))
-@dp.message(Command(commands=['инфо']))
+@dp.message(F.text.lower() == 'инфо')
 async def start(message: types.Message):
-    await message.answer('Этот бот предназначен для экспериментов с различными нейросетями.\n'
-                         'На данный момент бот ограничен в функционале.\n'
-                         'Доступные команды:\n'
-                         '/start /старт - приветственное сообщение\n'
-                         '/stop /стоп - прощальное сообщение\n'
-                         '/info /инфо - информация о боте\n'
-                         '/user /пользователь - выводит информацию о пользователе')
+    await message.answer(subrcs.get_info_message())
 
 @dp.message(Command(commands=['user']))
-@dp.message(Command(commands=['пользователь']))
+@dp.message(F.text.lower() == 'пользователь')
 async def start(message: types.Message):
     await message.answer(f'Данные пользователя, {message.from_user}!')
+
+@dp.message(F.text)
+async def msg(message: types.Message):
+    if 'привет' in message.text.lower():
+        await message.reply('И тебе привет!')
+    elif 'как дела' in message.text.lower():
+        await message.reply('Нормально, а у тебя?')
+    elif 'что ты умеешь' in message.text.lower() or 'что умеешь' in message.text.lower():
+        await message.reply(subrcs.get_info_message(), reply_markup=keyboard_001)
+    elif 'хорошо' in message.text.lower() or 'нормально' in message.text.lower():
+        await message.reply('Нормально, а у тебя?')
+    else:
+        await message.reply('Не понимаю тебя...')
+
 
 async def main():
     await dp.start_polling(bot)
